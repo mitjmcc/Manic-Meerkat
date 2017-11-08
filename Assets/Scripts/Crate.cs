82243a56df9e892740db3dcf28cc640ad345f6d8
MyNameIsNotPa <drawinsewell@yahoo.com>
2017-11-07 21:02:02 -0500

    More polish

---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour, IKillable {
+
+	public GameObject spawn;
+
+	void OnCollisionEnter(Collision col) {
+		var m = col.gameObject.GetComponent<MovementController>();
+		foreach (ContactPoint contact in col.contacts) {
+			Debug.Log(Vector3.Dot(transform.up, contact.normal));
+			float d = Vector3.Dot(transform.up, contact.normal);
+			if (d < 0 && d >= -1) {
+				// Check for player eventually
+				col.gameObject.GetComponent<Rigidbody>().velocity += 
+					Vector3.up * m.jumpAcceleration(1.5f, 1 + Time.deltaTime);
+				Kill();
+			} else if (d > 0 && d <=1) {
+				col.gameObject.GetComponent<Rigidbody>().velocity +=
+					Vector3.up * -m.jumpAcceleration(.005f, 1 + Time.deltaTime);
+				Kill();
+			}
+		}
+	}
+
+	public void Kill() {
+		// Instantiate(spawn, transform.position, Quaternion.identity);
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+	private bool gamePaused;
+	private GameObject main;
+	private ControllerUI uiHandler;
+	private Button resumeButton;
+
+	private bool selectButton;
+
+	void Start () {
+		gamePaused = false;
+		main = transform.Find ("Main").gameObject;
+		uiHandler = GameObject.FindObjectOfType<ControllerUI> ();
+		resumeButton = transform.Find ("Main/ResumeButton").GetComponent<Button> ();
+	}
+
+	void Update () {
+		if (selectButton) {
+			uiHandler.selectButton (resumeButton);
+			selectButton = false;
+			resumeButton.gameObject.GetComponent<Animator> ().SetBool("Highlighted", true);
+		}
+		if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.Joystick1Button7)) {
+			gamePaused = !gamePaused;
+			main.SetActive (gamePaused);
+			Time.timeScale = gamePaused ? 0 : 1;
+			selectButton = gamePaused;
+		}
+	}
+
+	public void Resume() {
+		gamePaused = false;
+		main.SetActive (gamePaused);
+		Time.timeScale = 1;
+	}
+}
