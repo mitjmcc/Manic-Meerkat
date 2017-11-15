a635f138caae45aa72218a3a4de36b19aa689eb2
IreneHuang <huangsss0815@gmail.com>
2017-11-15 09:34:46 -0500

    Text UI fixed
    Font applied texts. New text "Level 1" at top right corner.

---
+++ Assets/Scripts/fadeAwayScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class fadeAwayScript : MonoBehaviour
+{
+    float duration = 8;
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > duration)
+        {
+            Destroy(gameObject);
+        }
+
+        Text theText = GetComponent<Text>();
+        Color myColor = theText.color;
+        float ratio = Time.time / duration;
+        myColor.a = Mathf.Lerp(1, 0, ratio);
+        GetComponent<Text>().color = myColor;
+
+    }
+}
