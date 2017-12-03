025c1373c7638e0b5f879c719d42e71a0c999c3f
Mitch McClellan <mitchelljmcclellan@gmail.com>
2017-12-03 17:03:51 -0500

    Added the bullet boune back that Patrick wanted, but I forgot about

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour, IBashable {
+
+	Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+
+	public void OnBash() {
+		body.velocity = -body.velocity;
+	}
+}
