9bf852569c4ed9ab6c2b78ce86288abedc687403
Mitch McClellan <mitchelljmcclellan@gmail.com>
2017-12-09 23:04:06 -0500

    Add enemy models and animations, reorganize some assets into folders

--- Assets/Scripts/PacingEnemy.cs
+++ Assets/Scripts/PacingEnemy.cs
@@ -6,13 +6,16 @@ public class PacingEnemy : MonoBehaviour, IJumpable, IBashable {
 
 	public Transform[] points = new Transform[2];
 	public float speed;
+	Animator anim;
 	Vector3 target;
 	Vector3 start;
+	bool isDead;
 	float paceAmount;
 
 	void Start() {
 		target = points [0].position;
 		start = points [1].position;
+		anim = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -24,14 +27,32 @@ public class PacingEnemy : MonoBehaviour, IJumpable, IBashable {
 			target = start;
 			start = temp;
 		}
+		if (!isDead)
 		transform.position = Vector3.Lerp(start, target, paceAmount);
+		transform.LookAt(target);
 	}
 
 	public void OnJump() {
-		gameObject.SetActive(false);
+		if (!isDead) {
+			anim.SetTrigger("dead");
+			StartCoroutine("Death");
+		}
 	}
 
 	public void OnBash() {
-		gameObject.SetActive (false);
+		if (!isDead) {
+			anim.SetTrigger("dead");
+			StartCoroutine("Death");
+		}
+	}
+
+	IEnumerator Death() {
+		for (float f = 8f; f >= 0; f -= 0.1f) {
+			isDead = true;
+			GetComponent<Collider>().enabled = false;
+			if (f < .2f) 
+				transform.parent.gameObject.SetActive(false);
+			yield return null;
+		}
 	}
 }
